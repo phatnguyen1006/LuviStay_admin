3edacd0784000ec13f2dede035e857df35cf6498
P <19521998@gm.uit.edu.vn>
2022-04-15 11:17:57 +0700

    fix: Loader

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -3,7 +3,7 @@ import "./styles.scss";
 
 export const Loader: React.FC = () => {
 	return (
-		<div className="w-screen h-screen flex justify-center items-center">
+		<div className="loader-container">
 			<svg className="loader-svg" viewBox="0 0 50 50">
 				<circle className="ring" cx="25" cy="25" r="20"></circle>
 				<circle className="ball" cx="25" cy="5" r="3.5"></circle>
