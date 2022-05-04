a11f117b106468c922f77b22a1bb822100c3ab03
P <19521998@gm.uit.edu.vn>
2022-05-04 09:13:03 +0700

    Feat: Build DashBoard

--- src/pages/admin/Dashboard/index.tsx
+++ src/pages/admin/Dashboard/index.tsx
@@ -1,7 +1,66 @@
-// import "../../../../public/images/logo-square-primary.png";
+import { useState } from "react";
+import { Select } from "antd";
+const { Option } = Select;
+import Chart from "components/chart";
+import "./styles.scss";
+import { ChartType } from "components/chart/types";
+import { BsSliders } from "react-icons/bs";
 
 const DashBoard = (): JSX.Element => {
-    return <div className="_container">Layout</div>;
+  const [chartType, setchartType] = useState<ChartType>(ChartType.year);
+
+  function handleChange(value) {
+    setchartType(value);
+  }
+
+  return (
+    <div>
+      <section
+        className="dashboard-section-container"
+        style={{ backgroundColor: "red" }}
+      >
+        <h2>Dashboard</h2>
+        <p>This is my dashboard today information</p>
+      </section>
+      <section className="second-section">
+        <section
+          className="turnover-section-container"
+          style={{ backgroundColor: "cyan" }}
+        >
+          <div className="turnover-header">
+            <h2>Turn Over</h2>
+            <div className="custom-selection-btn-with-icon">
+              <div className="selection-icon-prefix">
+                <BsSliders />
+              </div>
+              <div className="custom-selection-btn">
+                <Select
+                  defaultValue={ChartType.year}
+                  className="selection-antd"
+                  suffixIcon={null}
+                  onChange={handleChange}
+                >
+                  <Option value={ChartType.year}>{ChartType.year}</Option>
+                  <Option value={ChartType.history}>{ChartType.history}</Option>
+                </Select>
+              </div>
+            </div>
+          </div>
+          <div className="chart-container">
+            <Chart chartType={chartType} />
+          </div>
+        </section>
+        <section className="news-view" style={{ backgroundColor: "salmon" }}>
+            <ul>
+                <li>LuviStay 1</li>
+                <li>LuviStay 2</li>
+                <li>LuviStay 3</li>
+                <li>LuviStay 4</li>
+            </ul>
+        </section>
+      </section>
+    </div>
+  );
 };
 
 export default DashBoard;
